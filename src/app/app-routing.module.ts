cef275ce735b9e35c0ded57cf72b02f172f74cf6
Reinaldo <lindolfodianda1@gmail.com>
2022-09-29 11:24:28 -0500

    original comit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+
+const routes:Routes=[
+  {path:'', redirectTo:'/login',pathMatch:'full'},
+  {path:'**',component:NopageFoundComponent}
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
